974e98210bd4f7c7bbecefa2dcdffea848e7a66e
lights9999 <32558210+lights9999@users.noreply.github.com>
2024-02-16 00:09:56 -0500

    made password secure

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -8,7 +8,7 @@
 #include "painlessMesh.h"
 
 #define   MESH_PREFIX     "ltngNetwork"
-#define   MESH_PASSWORD   "abc1234"
+#define   MESH_PASSWORD   "abc12349"
 #define   MESH_PORT       5555
 
 Scheduler userScheduler; // to control your personal task
